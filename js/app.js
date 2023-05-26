355878573c265181fb09503a7e8187790e5fb1f8
shoaib-ali-asif <eagleshebi@gmail.com>
2023-05-26 11:48:09 +0500

    changed multiple things

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// Find the area of a rectangle where the length is 5 and the width is 8. 
+ let length = 5;
+ let width = 10;
+ let AreaOfRectangle = length * width;
+ console.log (`AreaOfRectangle : ${AreaOfRectangle}` )
+
+
+// Find the area of a triangle where the base is 4 and the height is 3. 
+let base = 4;
+let height = 3;
+let AreaOfTriangle = base * height / 2;
+console.log (`AreaOfTriangle : ${AreaOfTriangle}` )
+
+// Find the area of a circle where the radius is 3.
+let radius = 3;
+let π = 3.14;
+let AreaOfCircle = π * radius * radius
+console.log (`AreaOfCircle : ${AreaOfCircle}` )
+
+// Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius.
+
+let Celsius = -17;
+let celciusToFahrenheit = (Celsius *  1.8 ) + 32
+
+let Fahrenheit = 0.4;
+let FahrenheitToCelsius = (Fahrenheit - 32 ) * 1.8
+console.log(`celciusToFahrenheit : ${celciusToFahrenheit} \nFahrenheitToCelsius : ${FahrenheitToCelsius}` )
